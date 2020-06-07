0d38020dbcd513c0665fc79c64c6977aca53d6e9
pblumel <moose392@gmail.com>
2020-06-07 17:50:06 -0600

    Clean up Vendor enum

--- src/SensorMessage.h
+++ src/SensorMessage.h
@@ -5,8 +5,8 @@
 #define NUM_CHANNELS 16
 
 
-enum vendors {UNKNOWN, HONEYWELL, TWOGIG};
-static const vendors vendor_channel_map[NUM_CHANNELS] = {	// A map of channel numbers to the associated vendors
+enum Vendor {UNKNOWN, HONEYWELL, TWOGIG};
+static const Vendor vendor_channel_map[NUM_CHANNELS] = {	// A map of channel numbers to the associated vendors
 		UNKNOWN, UNKNOWN, TWOGIG, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN,
 		HONEYWELL, TWOGIG, TWOGIG, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN};
 
@@ -17,14 +17,14 @@ public:
 	bool isProcessed() const {return processed_state;};
 	void newMessage() {processed_state = false;};
 	void idVendor(const unsigned char& channel) {vendor = vendor_channel_map[channel];};
-	vendors getVendor() const {return vendor;}
+	Vendor getVendor() const {return vendor;}
 	void setTXID(const unsigned int& txid) {this->txid = txid;};
 	unsigned int getTXID() const {return txid;};
 	void setState(const unsigned char& sensor_state) {this->sensor_state = sensor_state;};
 	unsigned char getState() const {return sensor_state;};
 private:
 	bool processed_state {true};
-	vendors vendor {UNKNOWN};
+	Vendor vendor {UNKNOWN};
 	unsigned int txid {};
 	unsigned char sensor_state {};
 };
